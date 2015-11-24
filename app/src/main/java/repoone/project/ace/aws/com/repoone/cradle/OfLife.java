9136c391b386a5d4365f7f65347a56b4086d7c4f
RJ <roljohn.villanueva@awsys-i.com>
2015-11-24 14:29:59 +0800

    Commit second dummy class

---
+++ app/src/main/java/repoone/project/ace/aws/com/repoone/cradle/OfLife.java
@@ -0,0 +1,8 @@
+package repoone.project.ace.aws.com.repoone.cradle;
+
+/**
+ * Created by roljohn.villanueva on 11/24/2015.
+ */
+public class OfLife {
+    public static String LIFE = "I AM ALIVE!!!";
+}
